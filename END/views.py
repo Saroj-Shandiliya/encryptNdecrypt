e80b45385a634cc92764afa7dae9004d1f7ed921
encryptndecrypt <encryptndecrypt@gmail.com>
2020-10-05 20:17:45 +0530

    sqlalchemy  commit

--- END/views.py
+++ END/views.py
@@ -47,7 +47,7 @@ eml=''
 #Engine created for SQLalchemy
 
 
-
+print(os.getenv("HEROKU_POSTGRESQL_GREEN_URL"))
 engine=create_engine(os.getenv("HEROKU_POSTGRESQL_GREEN_URL"))
 db=scoped_session(sessionmaker(bind=engine))
 
